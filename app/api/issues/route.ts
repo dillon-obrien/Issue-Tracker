d4dc14238ea7765eee0113a28e89c9db51c54014
dillon-obrien <dillon.m.obrien@outlook.com>
2023-11-15 00:22:05 -0700

    Build an api for creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,26 @@
+import prisma from "@/prisma/client";
+import { NextRequest,NextResponse } from "next/server";
+import {z} from 'zod';
+
+//Validate the body
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+})
+
+//The Request
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    //If bad data return 400 otherwise success!
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, {status: 400})
+
+    //Call to prisma to create a new issue item
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description}
+    })
+
+    //send back to the client
+    return NextResponse.json(newIssue,{status: 201})
+}
